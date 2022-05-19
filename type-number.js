6d08cbf3b1c719d3e6cf975553dfbc07fc3dba75
Mikael da Conceição Souza Luz <mikael.luz@fretebras.com.br>
2022-05-19 14:52:52 -0300

    Exemplos da aula 1

---
+++ type-number.js
@@ -0,0 +1,30 @@
+// Tipo number
+//
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+console.log(operacaoMatematica);
+
+
+// Tipo ponto flutuante
+//
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+
+// NaN (not a number) (não é um numero)
+//
+const alura = "alura";
+
+console.log(alura * primeiroNumero);
+
+// Infinity
+//
+const a = 10;
+const b = 0;
+console.log(a/b);
---
+++ type-strings.js
@@ -0,0 +1,24 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "SenhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao = 'Meu nome é ';
+const meuNome = "Mikael"
+
+//concetenação (usa sempre o operador '+' para somar textos)
+console.log(citacao + meuNome)
+
+//template string OU template literal
+// FAZER PESQUISA PARA SABER COMO UTILIZA-LOS
+
+
+//Comparando valores de strings
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false 
+
+//Contando a quantidade de caracteres numa string com LENGTH
+const senha2 = "minhasenha123";
+console.log(senha2.length);
