9afe56d08de969269c2122c6231e3fd95ffd2b0d
Mikael da Conceição Souza Luz <mikael.luz@fretebras.com.br>
2022-06-26 23:33:13 -0300

    template strings, erros e operador ternario

---
+++ template-strings.js
@@ -0,0 +1,14 @@
+const nome = "Ju";
+const idade = 2021-1981;
+const cidadeNascimento = "São Paulo";
+
+// const apresentacao = "meu nome é " + 
+//                     nome + ", minha idade é " + 
+//                     idade + ", minha cidade de nascimento é " + 
+//                     cidadeNascimento;
+
+const apresentacao = `meu nome é ${nome}, 
+                    minha idade é ${idade} 
+                    e nasci na cidade de ${cidadeNascimento}`;
+
+console.log(apresentacao)
